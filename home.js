bc3e3072da41c8af3150f9c856d730f3e74b87eb
Claudia <161829778+ClaudiaPezzoli@users.noreply.github.com>
2024-04-30 18:07:36 +0200

    NEW AND CORRECT COMMIT

--- home.js
+++ home.js
@@ -1,49 +1,20 @@
-const nav = document.querySelector('#nav');
-
-function fixNav(){ /*per mantenere navbar sopra mentre scrolla pagina*/
-    if(window.scollY>200){
-        nav.classList.add('is-scrolled');
-    }else{
-        nav.classList.remove('is-scrolled')
+class Prodotto{
+    constructor(img, nome, descrizione, brand, prezzo){
+        this.img = img; 
+        this.nome = nome;
+        this.descrizione = descrizione;
+        this.brand = brand;
+        this.prezzo = prezzo;
     }
 }
-window.addEventListener('scroll', fixNav);
 
+let prodotti = []; 
 
-let containerSmartphone = document.querySelector('.container-smartphone');
+function scegliProdotto(){
 
-const ULRSMARTPHONE = 'https://dummyjson.com/products/smatphones';
-
-let smartphone = [];
-
-fetch(ULRSMARTPHONE)
-
-    .then(data => {
-        return data.json();
-    })
-
-    .then(response => {
-        let smartphone = response.products; 
-        stampaSmartphone(smartphone);
-    })
-
-function stampaSmartphone(products){
-    products.forEach(prodotto => {
-        containerSmartphone.innerHTML =+ `<div class="swiper-slide">
-        <div class="card" style="width: 18rem;">
-            <img src="https://source.unsplash.com/600x400?black+cat" class="card-img-top" alt="">
-            <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">Lorem, ipsum</p>
-                <button id="btnMaggioriInfo" type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">Maggiori info</button>
-            </div>
-            </div>
-        </div>`
-    });
+    let prodottiScelti = ['https://dummyjson.com/products/search?q=phone', 'https://dummyjson.com/products/search?q=shoes', 'https://dummyjson.com/products/category/motorcycle', 'https://dummyjson.com/products/category/groceries']; 
 }
 
 
-//fetch per cercare un articolo all'interno dei telefoni
-fetch('https://dummyjson.com/products/search?q=phone')
-.then(res => res.json())
-.then(console.log);
+// scarpe al posto di cucine 
+// moto al posto di camere da letto
